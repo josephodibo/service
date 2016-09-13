1ba1bb2f4e89b251bee4ca6719994c170768197b
joseph odibobhahemen <josephodibo@gmail.com>
2016-09-13 01:26:46 -0500

    update

--- app/src/main/java/com/example/josephodibobhahemen/pocservice/service/TestService.java
+++ app/src/main/java/com/example/josephodibobhahemen/pocservice/service/TestService.java
@@ -5,12 +5,9 @@ import com.example.josephodibobhahemen.pocservice.manager.ServiceManager;
 import com.example.josephodibobhahemen.pocservice.serializer.ICustomTypeAdapterProvider;
 import com.google.gson.GsonBuilder;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
 
 /**
@@ -21,11 +18,12 @@ public class TestService {
 
     interface TestApi {
         @GET("api/test/service/user")
-        Call<List<String>> getUsers(List<String> reply);
+        Call<List<String>> getUsers();
     }
 
     TestApi api;
-    public  TestService() {
+
+    public TestService() {
         api = ServiceManager.getServiceInstance(TestApi.class, new ICustomTypeAdapterProvider() {
             @Override
             public void registerTypeAdapter(GsonBuilder gsonBuilder) {
@@ -35,8 +33,7 @@ public class TestService {
     }
 
     public void getTestUser(String operationId) {
-        List<String> strings = new ArrayList<>();
-        Call<List<String>> call = api.getUsers(strings);
+        Call<List<String>> call = api.getUsers();
         call.enqueue(new CustomCallBack<List<String>>(operationId));
     }
 }
